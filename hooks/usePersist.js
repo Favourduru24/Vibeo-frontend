4cb2b295fb4f980e4da506f46594c04d9ba2948f
Pristine duru <Favourduru24@gmail.com>
2025-09-12 20:03:13 -0700

    implemented the hook to persist login

---
+++ hooks/usePersist.js
@@ -0,0 +1,23 @@
+ 'use client'
+import { useState, useEffect } from 'react'
+
+const usePersist = () => {
+  const [persist, setPersist] = useState(true)
+
+  useEffect(() => {
+   const stored = localStorage.getItem('persist')
+  //  console.log("Stored persist value from localStorage:", stored)
+   if (stored !== null) {
+     setPersist(JSON.parse(stored))
+   }
+ }, [])
+ 
+ useEffect(() => {
+  //  console.log("Persist value changed:", persist)
+   localStorage.setItem('persist', JSON.stringify(persist))
+ }, [persist])
+
+  return [persist, setPersist]
+}
+
+export default usePersist
